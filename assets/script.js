2f075f67005f03442079ee2cfb35b5f1b682d401
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-13 08:43:49 -0300

    feat: function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,24 @@
 
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('min-hand');
+const hourHand = document.querySelector('hour-hand');
+
+
 function setDate(){
     const now = new Date();
+
     const seconds = now.getSeconds();
-    console.log(seconds);
+    const secondsDegrees = ((seconds / 60) *360) +90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees} deg)`;
+
+    const hour = now.getMinutes();
+    const hourDegreee = ((mins/12) *360) +90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
 }
 
 setInterval(setDate, 1000);
